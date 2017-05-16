b57c19bac3ba8d3899dbc48fa3ff757d6da76438
Marcos Maia <marcos.pais.alves.maia@ing.nl>
2017-05-16 14:21:57 +0200

    initial commit with mock-stocks spring boot app

---
+++ mock-stocks/src/main/java/com/codespair/kafka/mockstocks/MockStocksApplication.java
@@ -0,0 +1,12 @@
+package com.codespair.kafka.mockstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockStocksApplication.class, args);
+	}
+}
