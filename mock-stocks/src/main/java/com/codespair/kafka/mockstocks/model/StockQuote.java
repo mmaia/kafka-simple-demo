7c32d420e076edc1d563c23687e84f3e7a86d5b0
Marcos Maia <maia.marcos@gmail.com>
2017-05-16 22:51:26 +0200

    refining initial model

--- mock-stocks/src/main/java/com/codespair/kafka/mockstocks/model/StockQuote.java
+++ mock-stocks/src/main/java/com/codespair/kafka/mockstocks/model/StockQuote.java
@@ -4,9 +4,8 @@ import java.math.BigDecimal;
 
 public class StockQuote {
     private String symbol;
-
     private BigDecimal high;
     private BigDecimal low;
     private BigDecimal lastTrade;
-
+    private StockDetail stockDetail;
 }
