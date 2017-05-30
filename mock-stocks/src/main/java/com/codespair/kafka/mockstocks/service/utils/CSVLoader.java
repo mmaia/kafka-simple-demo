b65527fff664268adecf264e8aab68200cdcd6a3
Marcos Maia <marcos.pais.alves.maia@ing.nl>
2017-05-30 14:39:55 +0200

    fixing inconsistency after results of jacoco/sonarqube report

--- mock-stocks/src/main/java/com/codespair/kafka/mockstocks/service/utils/CSVLoader.java
+++ mock-stocks/src/main/java/com/codespair/kafka/mockstocks/service/utils/CSVLoader.java
@@ -19,28 +19,26 @@ import java.util.Map;
 public class CSVLoader {
 
     public Map loadExchangeCSV(String filePath) {
+        Map<String, StockDetail> result;
         ColumnPositionMappingStrategy<StockDetail> loadStrategy = new ColumnPositionMappingStrategy<>();
         loadStrategy.setType(StockDetail.class);
         String[] columns = new String[] {"symbol", "name", "lastSale", "marketCap", "ipoYear", "sector", "industry", "summaryQuote"}; // the fields to bind do in your JavaBean
         loadStrategy.setColumnMapping(columns);
         CsvToBean<StockDetail> csv = new CsvToBean<>();
-        CSVReader reader = createCSVReader(filePath);
-        return stockDetailsBySymbol(csv.parse(loadStrategy, reader));
-    }
-
-    private CSVReader createCSVReader(String filePath) {
-        CSVReader reader;
-        try {
+        try{
             File resource = new ClassPathResource(filePath).getFile();
-            reader = new CSVReader(new FileReader(resource));
-        } catch (Exception e) {
+            FileReader fileReader = new FileReader(resource);
+            CSVReader reader = new CSVReader(fileReader);
+            result = stockDetailsBySymbol(csv.parse(loadStrategy, reader));
+            fileReader.close();
+        } catch(Exception e) {
             log.error("Failed to load csv file with exchange information because: {}",  e.getMessage());
             throw new InvalidCSVPathException(e.getMessage());
         }
-        return reader;
+        return result;
     }
 
-    private Map stockDetailsBySymbol(List<StockDetail> stockDetailList) {
+    private Map<String, StockDetail> stockDetailsBySymbol(List<StockDetail> stockDetailList) {
         Map<String, StockDetail> stockDetailMap = new HashMap<>();
         stockDetailList.forEach((stockDetail) -> {
             stockDetailMap.put(stockDetail.getSymbol(), stockDetail);
