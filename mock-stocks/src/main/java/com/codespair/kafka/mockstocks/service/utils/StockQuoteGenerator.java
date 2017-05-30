42d2680e2e2d01c4eaabf9f9f23a555b54f148f5
Marcos Maia <marcos.pais.alves.maia@ing.nl>
2017-05-30 14:54:05 +0200

    added suppression for sonarqube as this, in the end, will be an infinite loop generating quotes indefinitely.

--- mock-stocks/src/main/java/com/codespair/kafka/mockstocks/service/utils/StockQuoteGenerator.java
+++ mock-stocks/src/main/java/com/codespair/kafka/mockstocks/service/utils/StockQuoteGenerator.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class StockQuoteGenerator {
 
+    @SuppressWarnings("squid:S2189")
     public void startQuoteGeneration() {
         while(true) {
+            throw new UnsupportedOperationException("Not yet implemented");
         }
     }
 
