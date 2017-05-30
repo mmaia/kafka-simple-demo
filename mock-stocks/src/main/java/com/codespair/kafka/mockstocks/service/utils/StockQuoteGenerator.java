0c967ff407ab5991e4f7a675cab22b0b382073bc
Marcos Maia <maia.marcos@gmail.com>
2017-05-30 20:59:42 +0200

    added comment to suppresswarning annotation.

--- mock-stocks/src/main/java/com/codespair/kafka/mockstocks/service/utils/StockQuoteGenerator.java
+++ mock-stocks/src/main/java/com/codespair/kafka/mockstocks/service/utils/StockQuoteGenerator.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class StockQuoteGenerator {
 
-    @SuppressWarnings("squid:S2189")
+    @SuppressWarnings("squid:S2189") // avoid being marked by check for infinite loop from sonarqube
     public void startQuoteGeneration() {
         while(true) {
             throw new UnsupportedOperationException("Not yet implemented");
