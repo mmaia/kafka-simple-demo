e5d52a7c1d21be236b8a3e9a945173e30ca09893
Marcos Maia <maia.marcos@gmail.com>
2017-06-04 02:44:53 +0200

    added stream dependency and support config class

--- mock-stocks/src/main/java/com/codespair/mockstocks/service/kafka/stream/KafkaStreamConfig.java
+++ mock-stocks/src/main/java/com/codespair/mockstocks/service/kafka/stream/KafkaStreamConfig.java
@@ -1,4 +1,38 @@
 package com.codespair.mockstocks.service.kafka.stream;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@EnableKafkaStreams
 public class KafkaStreamConfig {
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public StreamsConfig myKStreamBuilder() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "testStreams");
+        props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
+        props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
+        return new StreamsConfig(props);
+    }
+
+    @Bean
+    public KStream<?, ?> kStream(KStreamBuilder kStreamBuilder) {
+        KStream<Integer, String> stream = kStreamBuilder.stream("streamingTopic");
+        // Fluent KStream API
+        return stream;
+    }
 }
