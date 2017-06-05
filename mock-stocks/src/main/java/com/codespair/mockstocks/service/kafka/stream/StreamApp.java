50ca931b269c8d49c6348c555c0459daf738a1d1
Marcos Maia <maia.marcos@gmail.com>
2017-06-05 22:09:27 +0200

    streaming to console first working version.

--- mock-stocks/src/main/java/com/codespair/mockstocks/service/kafka/stream/StreamApp.java
+++ mock-stocks/src/main/java/com/codespair/mockstocks/service/kafka/stream/StreamApp.java
@@ -1,15 +1,17 @@
 package com.codespair.mockstocks.service.kafka.stream;
 
-import com.codespair.mockstocks.model.StockQuote;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -21,17 +23,16 @@ public class StreamApp {
 
     private KafkaStreams createStockQuoteStreamsInstance(String hosts) {
         log.info("loading kafka stream configuration");
-        final JsonSerializer<StockQuote> jsonSerializer = new JsonSerializer<>();
-        final JsonDeserializer<StockQuote> jsonDeserializer = new JsonDeserializer<>(StockQuote.class);
-        final Serde<StockQuote> serde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
+        final Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+        final Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+        final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
 
         KStreamBuilder kStreamBuilder = new KStreamBuilder();
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "mock-stocks-example-stream");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, hosts);
-        KStream<StockQuote, StockQuote> stockQuoteRaw = kStreamBuilder.stream(serde, serde, "stockQuoteTopic");
-        KStream<StockQuote, StockQuote> stockQuoteStreamed = stockQuoteRaw.through("stockQuotesStreamed");
-        stockQuoteStreamed.print();
+        KStream<JsonNode, JsonNode> stockQuoteRaw = kStreamBuilder.stream( jsonSerde, jsonSerde , "stockQuoteTopic");
+        stockQuoteRaw.print();
         return new KafkaStreams(kStreamBuilder, props);
     }
 
