bccecd0d2b6741e50a5e18c5c1dcede739d0abfb
Marcos Maia <maia.marcos@gmail.com>
2017-06-06 18:43:24 +0200

    cleanup removed unused imports

--- mock-stocks/src/main/java/com/codespair/mockstocks/service/kafka/stream/StreamEnrichProduceHighLevelApi.java
+++ mock-stocks/src/main/java/com/codespair/mockstocks/service/kafka/stream/StreamEnrichProduceHighLevelApi.java
@@ -20,7 +20,6 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
