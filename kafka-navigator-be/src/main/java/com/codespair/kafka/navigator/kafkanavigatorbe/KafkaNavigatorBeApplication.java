46cefda2567410934d7d8e0cf4d0e0914295faff
Marcos Maia <maia.marcos@gmail.com>
2017-10-13 21:56:54 +0200

    added be project for kafka-navigator

---
+++ kafka-navigator-be/src/main/java/com/codespair/kafka/navigator/kafkanavigatorbe/KafkaNavigatorBeApplication.java
@@ -0,0 +1,12 @@
+package com.codespair.kafka.navigator.kafkanavigatorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNavigatorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaNavigatorBeApplication.class, args);
+	}
+}
