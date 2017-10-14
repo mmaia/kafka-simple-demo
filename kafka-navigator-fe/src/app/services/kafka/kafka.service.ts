2caec0ce1148816e765e37f992d4d27c0c4a56a3
Marcos Maia <maia.marcos@gmail.com>
2017-10-14 20:43:26 +0200

    workin in progress in this service

--- kafka-navigator-fe/src/app/services/kafka/kafka.service.ts
+++ kafka-navigator-fe/src/app/services/kafka/kafka.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class KafkaService {
 
+  private kNavUrl: string = 'api/topics/';
   private kHosts: Array<string>;
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getTopics(): Promise<any> {
+    return this.http.get(this.kNavUrl)
+      .toPromise()
+      .then((response) => console.log(JSON.stringify(response)))
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
 }
